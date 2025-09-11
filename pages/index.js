d73badbb6bec5b9849f32fb2affc4ecef94a898e
Raishsh <raitobias777@gmail.com>
2025-09-11 19:39:52 -0300

    add: adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home (){
-  return <h1>Eu sou o melhor do mundo</h1>
+function Home() {
+  return <h1>Eu sou o melhor do mundo</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
 export default Home;
